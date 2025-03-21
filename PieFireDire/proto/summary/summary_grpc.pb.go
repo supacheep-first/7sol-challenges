6d5726fc636fc67cddb689a30cd8563c796eaf1d
supacheep-first <first.supacheep@gmail.com>
2025-03-21 15:26:58 +0700

    fix: regenerate proto

--- PieFireDire/proto/summary/summary_grpc.pb.go
+++ PieFireDire/proto/summary/summary_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: summary/summary.proto
+// source: summary.proto
 
 package summary
 
@@ -117,5 +117,5 @@ var SummaryService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "summary/summary.proto",
+	Metadata: "summary.proto",
 }
